0236e673f04d6cb9af0899a9a5e6cb2c520c7e24
bgasowski <bgasowski@gmail.com>
2017-01-12 23:10:21 +0100

    Fixed dependency installation script

--- packages.R
+++ packages.R
@@ -1,8 +1,9 @@
-devtools::install("package") #install TANBayes
 # install missing packages
-packageNames = c("rpart", "e1071", "C50", "RWeka")
+packageNames = c("rpart", "e1071", "C50", "RWeka", "devtools", "igraph")
 
 check.inst <- packageNames %in% row.names(installed.packages())
 for(p in packageNames[!check.inst]) {
   install.packages(p)
 }
+
+devtools::install("package") # install TANBayes
